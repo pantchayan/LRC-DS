17c0440f0cb9114fd0ce8d498e27cc70d0800ab0
pantchayan <pantchayan@gmail.com>
2021-09-21 18:38:58 +0530

    21 sept

--- DP/L002.java
+++ DP/L002.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class L002 {
     static Scanner sc = new Scanner(System.in);
-    // mcmt -> MINIMUM COST MAZE TRAVERSAL
-
+    // mcmt -> MINIMUM COST MAZE TRAVERSAL =========================================================================
     public static int mcmtM(int sr, int sc, int[][] maze, int[][] qb){
         int dr = maze.length-1;
         int dc = maze[0].length-1;
@@ -52,6 +51,8 @@ public class L002 {
         return qb[0][0];
     }
 
+
+    // GOLD MINE ===================================================================================================
     public static int goldMineTab(int[][] mine){
         int n = mine.length;
         int m = mine[0].length;
@@ -85,10 +86,93 @@ public class L002 {
         return ans; 
     }
 
+    // TARGET SUM SUBSET -> repetition not allowed =================================================================
+    public static boolean tssMemo(int idx, int cs, int tar, boolean[][] qb, int[] arr){
+        if(cs==tar) return true;
+        if(cs > tar || idx == arr.length) return false;
+        if(qb[idx][cs]) return true;
 
+        boolean inc = tssMemo(idx+1, cs+arr[idx], tar,qb, arr);
+        boolean exc = tssMemo(idx+1, cs, tar ,qb,arr);
 
+        return qb[idx][cs] = inc || exc;
+        // return qb[cs] = tss(idx+1, cs+arr[idx], tar,qb, arr) || tss(idx+1, cs, tar ,qb,arr);
+    }
 
+    public static boolean tssTab(int[] arr, int tar){
+        int n = arr.length;
+        boolean[][] qb = new boolean[n+1][tar+1];
+        qb[0][0] = true;
+        for(int i = 1; i <= n; i++){
+            int coin = arr[i-1];
+            for(int j=0; j <= tar; j++){
+                boolean exc = qb[i-1][j];
+                boolean inc = (j-coin>=0)?qb[i-1][j-coin]:false;
 
+                qb[i][j] = inc || exc;
+            }
+        }
+        print2D(qb);
+        return qb[n][tar];
+    }
+
+    // COIN CHANGE COMBINATIONS -> repetitions allowed =============================================================
+    public static int coinChangeCombTab(int[] coins, int tar){
+        int n = coins.length;
+        int[] qb = new int[tar+1];
+        for(int i=0;i<n;i++){
+            int coin = coins[i];
+            for(int t=0;t<=tar;t++){
+                qb[t] += (t-coin>=0)?qb[t-coin]:0;
+            }
+        }
+
+        return qb[tar];
+    }
+
+    // COIN CHANGE PERMUTATIONS -> repetitions allowed =============================================================
+    public static int coinChangePermTab(int[] coins, int tar){
+        int n = coins.length;
+        int[] qb = new int[tar+1];
+        qb[0] = 1;
+        for(int t=0;t<=tar;t++){
+            for(int i=0;i<n;i++){
+                int coin = coins[i];
+                qb[t] += (t-coin>=0)?qb[t-coin]:0;
+            }
+        }
+        return qb[tar];
+    }
+
+    // 01  KNAPSACK ================================================================================================
+    public static int knapsack01Tab(int[] v, int[] w, int cap){
+        int n = v.length;
+        int[][] qb = new int[n+1][cap+1];
+        for(int i=1;i<=n;i++){
+            for(int c=0;c<=cap;c++){
+                int inc = (c-w[i-1]>=0)?qb[i-1][c-w[i-1]] + v[i-1]:0;
+                int exc = qb[i-1][c];
+                
+                qb[i][c] = Math.max(inc, exc);
+            }
+        }
+        return qb[n][cap];
+    }
+
+    // UNBOUNDED KNAPSACK ==========================================================================================
+    public static int unboundedKS(int[] v, int[] w, int cap){
+        int n = v.length;
+        int[] qb = new int[cap+1];
+        for(int i=1;i<=n;i++){
+            for(int c=0;c<=cap;c++){
+                int inc = (c-w[i-1]>=0)?qb[c-w[i-1]] + v[i-1]:0;
+                int exc = qb[c];
+            
+                qb[c] = Math.max(inc, exc);
+            }
+        }
+        return qb[cap];
+    }
 
     public static void set1(){
         int[][] maze = take2Dinput();
@@ -104,15 +188,43 @@ public class L002 {
     }
 
     public static void set2(){
-        int[][] mine  = take2Dinput();
-        print2D(mine);
-        System.out.println("ANSWER : " +  goldMineTab(mine));
+        // int[][] mine  = take2Dinput();
+        // print2D(mine);
+        // System.out.println("ANSWER : " +  goldMineTab(mine));
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int tar = sc.nextInt();
+        boolean[][] qb = new boolean[arr.length][tar];
+        System.out.println(tssMemo(0, 0, tar, qb, arr));
+        
+        print2D(qb);
+
+        System.out.println("//=====================================//");
+        System.out.println(tssTab(arr, tar));
     }
 
+    public static void set3(){
+
+    }
     public static void main(String[] args) throws Exception {
         // set1();
-        set2();
+        // set2();
+        set3();
+    }
 
+
+
+    private static void print1D(boolean[] arr){
+        for(boolean ele: arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println();
     }
 
     private static int[][] take2Dinput(){
@@ -130,6 +242,15 @@ public class L002 {
     }
 
     private static void print2D(int[][] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    private static void print2D(boolean[][] arr) {
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr[0].length;j++){
                 System.out.print(arr[i][j]+" ");
